5b36dd5b7f57d84f86fbf9958ca34bb47214805f
Gorkapromero <drmoriartydiu@gmail.com>
2018-10-15 18:49:24 +0200

    Proyecto

---
+++ Videojuego Octubre 2018/Assets/06 - Pruebas/Pruebas Gorka/ControlBotonesMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class ControlBotonesMenu : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+	public void CargarescenaPlay(string Nivel1)
+    {
+		SceneManager.LoadScene("Nivel 1"); 
+    }
+	public void CargarescenaRanking(string Ranking)
+	{
+		SceneManager.LoadScene("Ranking"); 
+	}
+	public void CargarescenaAjustes(string Ajustes)
+	{
+		SceneManager.LoadScene("Ajustes"); 
+	}
+	public void CargarescenaCreditos(string Creditos)
+	{
+		SceneManager.LoadScene("Creditos"); 
+	}
+
+}
