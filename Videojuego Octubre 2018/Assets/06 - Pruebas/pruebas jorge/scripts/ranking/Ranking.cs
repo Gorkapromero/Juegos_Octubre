c1327b4a4c5b3772dc322e1e69fa4430ee1018cc
JPalacio3D <jorge.jp92@gmail.com>
2018-10-19 20:29:36 +0200

    ranking parte 1

---
+++ Videojuego Octubre 2018/Assets/06 - Pruebas/pruebas jorge/scripts/ranking/Ranking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Ranking
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public int Score { get; set; }
+    public int Tiempo { get; set; }
+
+    public Ranking(int id, string name, int score, int tiempo)
+    {
+        this.Id = id;
+        this.Nombre = name;
+        this.Score = score;
+        this.Tiempo = tiempo;
+    }
+}
---
+++ Videojuego Octubre 2018/Assets/06 - Pruebas/pruebas jorge/scripts/ranking/RankingManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Data;
+using System.IO;
+using Mono.Data.Sqlite;
+using System;
+
+public class RankingManager : MonoBehaviour
+{
+    public string rutaDB;
+    public string Conexion;
+    public string nombreDB;
+
+    IDbConnection conexionDB;
+    IDbCommand comandosDB;
+    IDataReader leerDatos;
+
+    private List<Ranking> rankings;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+    void AbrirDB()
+    {
+        rutaDB = Application.dataPath + "/StreamingAssets/" + nombreDB;
+        Conexion = "URI=file:" + rutaDB;
+        conexionDB = new SqliteConnection(Conexion);
+        conexionDB.Open();
+    }
+	void ObtenerRanking()
+    {
+        rankings.Clear();
+        AbrirDB();
+        comandosDB = conexionDB.CreateCommand();
+        string sqlQuery = "selct * from Ranking";
+        comandosDB.CommandText = sqlQuery;
+
+        leerDatos = comandosDB.ExecuteReader();
+        while (leerDatos.Read())
+        {
+            rankings.Add(new Ranking(leerDatos.GetInt32(0), 
+                                     leerDatos.GetString(1), 
+                                     leerDatos.GetInt32(2), 
+                                     leerDatos.GetInt32(3)));
+        }
+        leerDatos.Close();
+        leerDatos = null;
+        CerrarDB();
+    }
+
+    void InsertarPuntos(string n, string s)
+    {
+        AbrirDB();
+        comandosDB = conexionDB.CreateCommand();
+        string sqlQuery = String.Format("insert into Ranking(Nombre,Score) values(\"{0}\",\"{1}\")",n,s);
+        comandosDB.CommandText = sqlQuery;
+
+        comandosDB.ExecuteScalar();
+        CerrarDB();
+    }
+
+    void BorrarPuntos(int id)
+    {
+        AbrirDB();
+        comandosDB = conexionDB.CreateCommand();
+        string sqlQuery = "delete from Ranking where PlayerId =" + id + "\"";
+        comandosDB.CommandText = sqlQuery;
+
+        comandosDB.ExecuteScalar();
+        CerrarDB();
+    }
+
+    void CerrarDB()
+    {
+        comandosDB.Dispose();
+        comandosDB = null;
+        conexionDB.Close();
+        conexionDB = null;
+    }
+}
