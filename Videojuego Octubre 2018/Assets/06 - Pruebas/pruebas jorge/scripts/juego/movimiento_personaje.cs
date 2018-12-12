77b432c33830ea3b0dd6b5871bbc3e69f5dcdcda
dn1rscn <dn1.rscn@gmail.com>
2018-12-12 02:23:44 +0100

    Varios_Anims+ControladorProta ("Espacio" para saltar)
    1- Animaciones de caidas para el Prota
    2- Actualizacion del Script de movimiento para el Prota
    3- Animator + BlendTree para mecanicas del Prota

--- Videojuego Octubre 2018/Assets/06 - Pruebas/pruebas jorge/scripts/juego/movimiento_personaje.cs
+++ Videojuego Octubre 2018/Assets/06 - Pruebas/pruebas jorge/scripts/juego/movimiento_personaje.cs
@@ -13,43 +13,58 @@ public class movimiento_personaje : MonoBehaviour
     public GameObject Finpartida;
     Rigidbody rb;
 
-	// Use this for initialization
-	void Start ()
+    bool Saltando;
+    bool salto;
+
+    // Use this for initialization
+    void Start ()
     {
         joystick = FindObjectOfType<Joystick>();
         rb = GetComponent<Rigidbody>();
+        //var rigidbody = GetComponent<Rigidbody>();
     }
 
-    private void Awake()
-    {
-       
-    }
 
     // Update is called once per frame
     void Update ()
     {
 
-        var rigidbody = GetComponent<Rigidbody>();
+        
         velocidad_fin = vidas.value * 20;
-        rigidbody.velocity = new Vector3(joystick.Horizontal * velocidad_fin,
-                                         rigidbody.velocity.y,
+        rb.velocity = new Vector3(joystick.Horizontal * velocidad_fin,
+                                         rb.velocity.y,
                                          0);
+        if (Saltando)
+        {
+            rb.velocity = new Vector3(joystick.Horizontal* vidas.value * 70, rb.velocity.y, 0);
+        }
 
-        if(joystick.Horizontal<0)
+        //Para el movimiento
+        GetComponent<Animator>().SetFloat("Speed", joystick.Horizontal);
+
+        //Para el salto
+        bool salto = Input.GetKeyDown(KeyCode.Space);
+        
+            GetComponent<Animator>().SetBool("Salto", salto);
+        
+
+        //Para la orientacion del Prota
+        if (joystick.Horizontal<0)
         {
             transform.eulerAngles = new Vector3(0, -90, 0);
-            GetComponent<Animation>().Play("Take 001 (1)");
+            //GetComponent<Animator>().Play("Take 001 (1)");
         }
         if (joystick.Horizontal>0)
         {
             transform.eulerAngles = new Vector3(0, 90, 0);
-            GetComponent<Animation>().Play("Take 001 (1)");
+            //GetComponent<Animator>().Play("Take 001 (1)");
         }
         if (joystick.Horizontal==0)
         {
             transform.eulerAngles = new Vector3(0,-180, 0);
-            GetComponent<Animation>().Play("Take 001");
+            //GetComponent<Animator>().Play("Take 001");
         }
+        
 
         if (vidas.value == vidas.minValue)
         {
@@ -60,9 +75,25 @@ public class movimiento_personaje : MonoBehaviour
         }
     }
 
+    public void saltoAdelante()
+    {
+        Saltando = true;
+        print("Saltando = TRUE");
+
+    }
+
+    public void finSaltoAdelante()
+    {
+        Saltando = false;
+        print("Saltando = FALSE");
+        rb.velocity = new Vector3(0, 0, 0);
+        GetComponent<Animator>().SetBool("Salto", false);
+
+    }
+
     public void Stop()
     {
         rb.velocity = new Vector3(0,0,0);
-        GetComponent<Animation>().Play("Take 001");
+        GetComponent<Animator>().Play("Caida_Atras");
     }
 }
