d9862a400ffdae53e1aac1ab06133e1d01129afc
Inigo21 <inigofraile@gmail.com>
2019-01-20 15:44:23 +0100

    Ajustes_Menu

---
+++ Videojuego Octubre 2018/Assets/06 - Pruebas/Pruebas_Inigo/SCRIPTS/scp_Anim_Menu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scp_Anim_Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void _ActivarAnim()
+    {
+        StartCoroutine(AnimMenuTaza());
+    }
+
+
+    IEnumerator AnimMenuTaza()
+    {
+        yield return new WaitForSeconds(1.7f);
+        GameObject.Find("Taza").GetComponent<Animator>().Play("Caida_Atras 0");
+        yield return new WaitForSeconds(1.5f);
+        GameObject.Find("Taza").GetComponent<Animator>().CrossFade("Movimiento",0.5f);
+    }     
+}
