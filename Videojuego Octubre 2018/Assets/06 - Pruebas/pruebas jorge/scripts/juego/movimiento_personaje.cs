f7c3bee7486f03dc6e57a222e92125a9ddc9b9a6
dn1rscn <dn1.rscn@gmail.com>
2019-01-24 03:27:01 +0100

    Metiendo Enemigos
    (AÚN POR TERMINAR)
    - Metido en escena el Malva Explosivo
    - Metido en escena el Malva Pegajoso
    - He repasado las animaciones del Malva Pegajoso y la textura
    - Prueba para apañar la animacion solapada de andar y correr de la taza

--- Videojuego Octubre 2018/Assets/06 - Pruebas/pruebas jorge/scripts/juego/movimiento_personaje.cs
+++ Videojuego Octubre 2018/Assets/06 - Pruebas/pruebas jorge/scripts/juego/movimiento_personaje.cs
@@ -28,43 +28,57 @@ public class movimiento_personaje : MonoBehaviour
     // Update is called once per frame
     void Update ()
     {
-
         
         //velocidad_fin = vidas.value * 20;
         rb.velocity = new Vector3(joystick.Horizontal * velocidad_fin,
                                          rb.velocity.y,
                                          0);
-        if (Saltando)
+        if (Saltando) //La variable "Saltando" se activa y desactiva en un evento de la animación de saltar adelante
         {
-            rb.velocity = new Vector3(joystick.Horizontal* vidas.value * 30, rb.velocity.y, 0);
+            rb.velocity = new Vector3(joystick.Horizontal * vidas.value * 35, rb.velocity.y, 0);
         }
 
-        //Para el movimiento
-        GetComponent<Animator>().SetFloat("Speed", joystick.Horizontal);
-
+        //Para las animaciones de movimiento
+              //***Corrección "temporal" del solapado de animaciones de andar y correr ********
+                if (joystick.Horizontal < 0.62 && joystick.Horizontal > 0.34)
+                {
+                    GetComponent<Animator>().SetFloat("Speed", 0.34f);
+                }
+                else if (joystick.Horizontal > -0.62 && joystick.Horizontal < -0.34)
+                {
+                    GetComponent<Animator>().SetFloat("Speed", -0.34f);
+                }
+             //*********************************************************************************
+        else
+        {
+            GetComponent<Animator>().SetFloat("Speed", (float)System.Math.Round(joystick.Horizontal, 2));
+        }
+        
         //Para el salto
         bool salto = Input.GetKeyDown(KeyCode.Space);
         
             GetComponent<Animator>().SetBool("Salto", salto);
-        
+
 
         //Para la orientacion del Prota
-        if (joystick.Horizontal<0)
+        if (!Saltando)
         {
-            transform.eulerAngles = new Vector3(0, -90, 0);
-            //GetComponent<Animator>().Play("Take 001 (1)");
+            if (joystick.Horizontal < 0)
+            {
+                transform.eulerAngles = new Vector3(0, -90, 0);
+                //GetComponent<Animator>().Play("Take 001 (1)");
+            }
+            if (joystick.Horizontal > 0)
+            {
+                transform.eulerAngles = new Vector3(0, 90, 0);
+                //GetComponent<Animator>().Play("Take 001 (1)");
+            }
+            if (joystick.Horizontal == 0)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                //GetComponent<Animator>().Play("Take 001");
+            }
         }
-        if (joystick.Horizontal>0)
-        {
-            transform.eulerAngles = new Vector3(0, 90, 0);
-            //GetComponent<Animator>().Play("Take 001 (1)");
-        }
-        if (joystick.Horizontal==0)
-        {
-            transform.eulerAngles = new Vector3(0,-180, 0);
-            //GetComponent<Animator>().Play("Take 001");
-        }
-        
 
         if (vidas.value == vidas.minValue)
         {
