8fdc72e3417418edcd2db800744f6cdac75f0176
JPalacio3D <jorge.jp92@gmail.com>
2019-11-05 20:25:47 +0100

    varios
    -cajas daño
    -ataque pegajoso

--- Videojuego Octubre 2018/Assets/06 - Pruebas/pruebas jorge/scripts/juego/enemigos/ataquePegajoso.cs
+++ Videojuego Octubre 2018/Assets/06 - Pruebas/pruebas jorge/scripts/juego/enemigos/ataquePegajoso.cs
@@ -12,12 +12,12 @@ public class ataquePegajoso : MonoBehaviour
 	Transform jugador;
 
 	public GameObject prefabPoff;
-	public float Altura;
+	//public float AlturaMax;
 	public float Velocidad;
-	public Vector3 OffSetPosFinal;
+	//public Vector3 distancia;
 
 	// Use this for initialization
-	IEnumerator Start () 
+	void Start () 
 	{
 		Personaje = GameObject.FindGameObjectWithTag("Jugador").GetComponent<movimiento_personaje>();
 		Rb=GetComponent<Rigidbody>();
@@ -25,10 +25,12 @@ public class ataquePegajoso : MonoBehaviour
 
 		/*Vector3 moveDirection = (jugador.position-transform.position).normalized * FuerzaLanzamiento;
 		Rb.velocity = new Vector3(moveDirection.x,FuerzaLanzamiento,0);*/
-		while(true)
+		/*while(true)
 		{
-			yield return StartCoroutine(ataque(jugador,Altura,Velocidad));
-		}
+			yield return StartCoroutine(ataque(jugador.position,transform.position,AlturaMax,Velocidad));
+		}*/
+		Vector3 Vo = CalculateVelocity(jugador.position,transform.position, Velocidad);
+		Rb.velocity = Vo;
 	}
 	
 	// Update is called once per frame
@@ -46,24 +48,55 @@ public class ataquePegajoso : MonoBehaviour
 			break;
 
 			case "Suelo":
-			Vector3 PosPoff = new Vector3(transform.position.x, transform.position.y+15.8f, transform.position.z);
+			Vector3 PosPoff = new Vector3(transform.position.x, transform.position.y+3.3f, transform.position.z);
             GameObject poff = Instantiate(prefabPoff, PosPoff, Quaternion.identity);
             Destroy(this.gameObject);
 			break;
 		}
 	}
 
-	IEnumerator ataque(Transform objetivo,float altura,float velocidad)
+	IEnumerator ataque(Vector3 objetivo,Vector3 origen,float altura,float velocidad)
 	{
 		Vector3 StartPos = transform.position;
-		Vector3 endPos = objetivo.transform.position+OffSetPosFinal;
+		Vector3 endPos = objetivo;
+		float Dist = Vector3.Distance(StartPos,endPos);
+		float _altura = velocidad*(Dist/velocidad)-0.5f*Mathf.Abs(Physics.gravity.y)*((Dist/velocidad)*(Dist/velocidad));
 		float normalizedTime = 0.0f;
 		while(normalizedTime < 1.0f)
 		{
-			float yoffset = altura * 4.0f * (normalizedTime - normalizedTime*normalizedTime);
+			
+			Vector3 distancia = objetivo - origen;
+			float DistY = distancia.y;
+			float DistXZ = distancia.magnitude;
+			float tiempo = DistXZ/velocidad;
+			float yoffset = _altura * 4.0f * (normalizedTime - normalizedTime*normalizedTime);
 			transform.position = Vector3.Lerp(StartPos,endPos,normalizedTime)+yoffset*Vector3.up;
-			normalizedTime += Time.deltaTime/velocidad;
+			
+			normalizedTime += Time.deltaTime/(DistXZ/velocidad);
 			yield return null;
 		}
+	}
+
+	Vector3 CalculateVelocity(Vector3 objetivo, Vector3 origen, float _velocidad)
+	{
+		//definimos destancia x e y
+		Vector3 distancia = objetivo - origen;
+		Vector3 distanciaXZ = distancia;
+		distanciaXZ.y = 0f;
+
+		//creamos float representado la siatncia
+		float Sy = distancia.y;
+		
+		float Sxz = distanciaXZ.magnitude;
+		float tiempo = Sxz/_velocidad;
+
+		float Vxz = Sxz / tiempo;
+		float Vy = Sy/tiempo + 0.5f * Mathf.Abs(Physics.gravity.y)*tiempo;
+
+		Vector3 result = distanciaXZ.normalized;
+		result *= Vxz;
+		result.y = Vy;
+
+		return result;
 	}
 }
