0a2bf6745b1ab2f05abc6457e516836bc99f0da5
Gorkapromero <drmoriartydiu@gmail.com>
2020-02-02 15:28:52 +0100

    prefabs cofres mas escena logo

---
+++ Videojuego Octubre 2018/Assets/06 - Pruebas/Pruebas Gorka/Scripts/partcilesrevser.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem)), ExecuteInEditMode]
+
+public class partcilesrevser : MonoBehaviour
+{
+    ParticleSystem s;
+    public float curTime = 0;
+    public float speed;
+    public uint seed;
+
+    // Use this for initialization
+    void Start()
+    {
+        s = GetComponent<ParticleSystem>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (curTime <= 0)
+        {
+            curTime = s.main.duration;
+        }
+        s.randomSeed = seed;
+        s.Simulate(curTime);
+        curTime -= speed * Time.deltaTime;
+    }
+}
