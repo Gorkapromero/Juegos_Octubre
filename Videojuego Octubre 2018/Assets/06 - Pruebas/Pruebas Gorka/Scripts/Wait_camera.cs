7f554521ffc0434cf92cc5f89ba465e26e270dbb
Gorkapromero <drmoriartydiu@gmail.com>
2020-02-11 13:13:57 +0100

    Opening

---
+++ Videojuego Octubre 2018/Assets/06 - Pruebas/Pruebas Gorka/Scripts/Wait_camera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Wait_camera : MonoBehaviour
+{
+    public float wait_time = 30f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Wait());
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(wait_time);
+
+        SceneManager.LoadScene(1);
+    }
+}
